832b7d981704779369a4f0f60c4ef5066eeab76c
Valery <horrorofabyss@gmail.com>
2023-11-01 16:29:35 +0600

    edit typo

--- conftest.py
+++ conftest.py
@@ -9,7 +9,6 @@ def driver(request):
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--window-size=1920,1080")
-    options.add_experimental_option('excludeSwitches', ['enable-logging'])
     driver = webdriver.Chrome(options=options)
 
     request.cls.driver = driver
